fecefee1227e15124ba7dcac1bb56a983ca27757
Abdulbaki Suraj <codelikesuraj@gmail.com>
2024-05-27 20:40:16 +0100

    update: add API key

--- main.go
+++ main.go
@@ -7,23 +7,33 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"slices"
 	"strings"
 	"time"
 
 	"github.com/google/generative-ai-go/genai"
-	"github.com/joho/godotenv"
 	"google.golang.org/api/option"
 )
 
 const MODEL_NAME = "gemini-pro"
 
-func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading environment variables")
-	}
+var EXIT_CMD = []string{"EXIT", "QUIT", "exit", "quit"}
 
-	api_key := os.Getenv("GEMINI_API_KEY")
+func main() {
+	clearScreen()
+
+	fmt.Print("-----------------------------------------------")
+	fmt.Print("\nWelcome to the Gemini AI REPL by CODELIKESURAJ\n")
+	fmt.Print("\nCommands:\n")
+	fmt.Print("\t'QUIT', 'EXIT': end the program\n")
+	fmt.Print("\t'UPDATE_KEY':   update API key\n")
+	fmt.Print("------------------------------------------------\n\n")
+
+	api_key, ok := os.LookupEnv("GEMINI_API_KEY")
+	if !ok || api_key == "" {
+		updateApiKey()
+		api_key = os.Getenv("GEMINI_API_KEY")
+	}
 
 	ctx := context.Background()
 
@@ -32,17 +42,6 @@ func main() {
 		log.Fatal(err)
 	}
 	defer client.Close()
-
-	// clear the screen
-	cmd := exec.Command("clear")
-	cmd.Stdout = os.Stdout
-	cmd.Run()
-
-	fmt.Print("----------------------------------------------")
-	fmt.Print("\nWelcome to the Gemini AI REPL by CODELIKESURAJ\n")
-	fmt.Print("\nUsage:\n")
-	fmt.Print("\tType 'EXIT' or 'QUIT' to end the program\n")
-	fmt.Print("----------------------------------------------\n\n")
 
 	runRepl(ctx, client.GenerativeModel(MODEL_NAME).StartChat())
 }
@@ -59,8 +58,7 @@ func runRepl(ctx context.Context, cs *genai.ChatSession) {
 			continue
 		}
 
-		switch cleaned {
-		case "EXIT", "QUIT":
+		if slices.Contains(EXIT_CMD, cleaned) {
 			fmt.Print("\nGemini AI > Session ended!!!\n\n")
 			fmt.Print("----------------------------------------------\n")
 			os.Exit(0)
@@ -101,4 +99,37 @@ func printResp(resp *genai.GenerateContentResponse) {
 	}
 
 	fmt.Println()
+}
+
+func updateApiKey() {
+	clearScreen()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Enter your gemini API key > ")
+		scanner.Scan()
+		text := scanner.Text()
+		trimmed := strings.TrimSpace(text)
+
+		switch {
+		case trimmed == "":
+			clearScreen()
+			fmt.Print("Error: please enter a valid key!\n")
+			continue
+		case slices.Contains(EXIT_CMD, trimmed):
+			os.Exit(0)
+		}
+
+		clearScreen()
+
+		os.Environ()
+		os.Setenv("GEMINI_API_KEY", trimmed)
+		break
+	}
+}
+
+func clearScreen() {
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
